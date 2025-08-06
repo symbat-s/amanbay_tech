510291233683d32b95c9414e767ca697a17c68ef
marksolo001 <you@example.com>
2025-08-06 16:07:37 +0500

    new

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,8 +1,10 @@
 import Banner from "./components/Banner";
+import BannerTwo from "./components/BannerTwo";
 export default function Home() {
   return (
     <div className="">
       <Banner />
+      <BannerTwo />
     </div>
   );
 }
