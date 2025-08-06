348b89db64f3ba9b55f727b1d843e22931030b87
symbat-s <sssymbats0@gmail.com>
2025-08-06 17:18:40 +0500

    footer

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,6 +1,7 @@
 import Banner from "./components/Banner";
 import BannerTwo from "./components/BannerTwo";
 import AboutBanner from "./components/AboutSection";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Banner />
       <BannerTwo />
       <AboutBanner/>
+      <Footer/>
     </div>
   );
 }
