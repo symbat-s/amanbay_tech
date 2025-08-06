e5d0405a98cb032ecf30ad3902f32ab24937e1c2
symbat-s <sssymbats0@gmail.com>
2025-08-06 16:30:34 +0500

    about

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,10 +1,13 @@
 import Banner from "./components/Banner";
 import BannerTwo from "./components/BannerTwo";
+import AboutBanner from "./components/AboutSection";
+
 export default function Home() {
   return (
     <div className="">
       <Banner />
       <BannerTwo />
+      <AboutBanner/>
     </div>
   );
 }
