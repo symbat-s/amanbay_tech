02cf62150cca5d3b7ade4fbb0280d50bf546f02b
marksolo001 <you@example.com>
2025-08-09 18:47:36 +0500

    new

--- my-app/app/components/BannerTwo.tsx
+++ my-app/app/components/BannerTwo.tsx
@@ -1,7 +1,6 @@
-import React from "react";
-
+import { useState } from "react";
 const BannerTwo: React.FC = () => {
-  return (
+   return (
     <div className="relative w-full flex justify-center py-20 px-10">
    
       <div className="absolute top-0 right-8  transform -translate-x-1/2 z-0 ">
@@ -11,8 +10,8 @@ const BannerTwo: React.FC = () => {
       <div className="absolute top-0 right-8  transform -translate-x-1/2 z-0 ">
         <img src="foto/item2.png" alt="фон" className=" w-[1200px] h-[1200px] max-w-none max-h-none" />
       </div>
- 
-       <div className="w-[45px] h-[45px] rounded-full  border-[#00000014] border-[1px] bottom-[850px] mr-[1285px] absolute bg-[white] text-black text-center text-2xl flex items-center justify-center z-30">
+      <div className="">
+         <div className="w-[45px] h-[45px] rounded-full  border-[#00000014] border-[1px] bottom-[850px] mr-[1285px] absolute bg-[white] text-black text-center text-2xl flex items-center justify-center z-30">
             1
           </div>
 
@@ -23,12 +22,14 @@ const BannerTwo: React.FC = () => {
           <div className="w-[45px] h-[45px] rounded-full  border-[#00000014] border-[1px] bottom-[230px] mr-[1285px] absolute bg-[white] text-black text-center text-2xl flex items-center justify-center z-30">
             3
           </div>
+      </div>
+      
 
-      <div className="relative z-20 flex flex-col gap-[130px] right-[110px]">
+      <div id="1" className="relative z-20 flex flex-col gap-[130px] right-[110px] " >
      
         <div className="relative flex items-start gap-4">
           
-          <div className="bg-white border border-[#00000014] p-[20px] w-[490px] h-[180px]">
+          <div className="bg-white border border-[#00000014] p-[20px] w-[490px] h-[180px] transition-transform duration-500 ease-in-out hover:scale-110">
             <h1 className="text-[#505050] text-[25px] font-medium">Technical expertise</h1>
             <p className="font-light text-[#505050] mt-[8px]">
               Our team is proficient in modern programming languages, frameworks, and architectures.
@@ -40,7 +41,7 @@ const BannerTwo: React.FC = () => {
 
         <div className="relative flex items-start gap-4 ml-[100px]">
          
-          <div className="bg-white border border-[#00000014] p-[20px] w-[490px] h-[180px]">
+          <div className="bg-white border border-[#00000014] p-[20px] w-[490px] h-[180px] transition-transform duration-500 ease-in-out hover:scale-110">
             <h1 className="text-[#505050] text-[25px] font-medium">Customized approach</h1>
             <p className="font-light text-[#505050] mt-[8px]">
               We start with a deep understanding of your business objectives and create solutions
@@ -51,7 +52,7 @@ const BannerTwo: React.FC = () => {
 
         <div className="relative flex items-start gap-4">
           
-          <div className="bg-white border border-[#00000014] p-[20px] w-[490px] h-[180px]">
+          <div className="bg-white border border-[#00000014] p-[20px] w-[490px] h-[180px] transition-transform duration-500 ease-in-out hover:scale-110">
             <h1 className="text-[#505050] text-[25px] font-medium">Accountability for results</h1>
             <p className="font-light text-[#505050] mt-[8px]">
               We don’t just deliver a task; we take responsibility for the project’s success. Timely
@@ -77,4 +78,4 @@ const BannerTwo: React.FC = () => {
   );
 };
 
-export default BannerTwo;
+export default BannerTwo; 
