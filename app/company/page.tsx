c7208e7617dc1868a73218ad55277bf8061dc8e5
symbat-s <sssymbats0@gmail.com>
2025-08-15 10:38:52 +0500

    changed

---
+++ app/company/page.tsx
@@ -0,0 +1,11 @@
+import Header from "../components/Header";
+import Academy from "./components/Academy";
+
+export default function Company () {
+    return (
+        <div>
+            <Header />
+            <Academy/>
+        </div>
+    )
+}
