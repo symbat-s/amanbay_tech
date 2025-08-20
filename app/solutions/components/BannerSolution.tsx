9a502f571a79d35c63c1ee72a88698093f29043b
A.Symbat <symbat_2023@mail.ru>
2025-08-20 12:26:11 +0600

    new

--- app/solutions/components/BannerSolution.tsx
+++ app/solutions/components/BannerSolution.tsx
@@ -1,8 +1,8 @@
 export default function SolutionBanner() {
     return(
-        <div className="bg-[url(/foto/BannerSolution.png)] lg:mt-[80px] bg-center bg-cover h-[695px] py-2 relative w-full   md:pl-23">
-            <h1 className="mt-50 ml-40  text-4xl font-roboto">Ваш IT-Партнер</h1>
-            <p className="w-[622px] h-[140px] font-roboto ml-20 text-2xl mt-15">Мы создаем цифровые решения, которые <br/> заставляют ваш бизнес работать как часы.<br/> От здравоохранения до туризма, мы знаем,<br/> <span className="ml-15">как сделать вас лучше</span> </p>
+        <div className="bg-[url(/foto/BannerSolution.png)] lg:mt-[80px] bg-center bg-cover md:h-[695px] py-2 relative w-full h-[300px]  md:pl-23">
+            <h1 className="md:mt-50 mt-20 md:ml-40 px-5 text-lg md:text-4xl font-bold md:font-roboto">Ваш IT-Партнер</h1>
+            <p className="md:w-[622px] w-[250px] md:h-[140px] font-roboto md:ml-20 px-5 text-sm md:text-2xl mt-5 md:mt-15">Мы создаем цифровые решения, которые  заставляют ваш бизнес работать как часы.От здравоохранения до туризма, мы знаем, как сделать вас лучше </p>
         </div>
     )
 }
