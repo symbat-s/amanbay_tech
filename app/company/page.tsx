9b4d7cfa7606ea881119a530dbeaa5135aa3c1b2
A.Symbat <symbat_2023@mail.ru>
2025-08-20 15:21:30 +0600

    new

--- app/company/page.tsx
+++ app/company/page.tsx
@@ -1,15 +1,12 @@
 import Header from "../components/Header";
 import Employee from "./components/Employees";
-import WhoWeAre from "./components/WhoWeAre";
-import Academy from "./components/WhoWeAre";
+
 
 export default function Company () {
     return (
         <div>
             <Header />
-            {/* <WhoWeAre/>  */}
             <Employee/>
-            {/* <Academy/> */}
         </div>
     )
 }
