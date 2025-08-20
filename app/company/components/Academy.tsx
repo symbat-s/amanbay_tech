ba2c37bed2a8f7abc1e187cfc0b5cd5938f40587
A.Symbat <symbat_2023@mail.ru>
2025-08-20 21:18:30 +0600

    new

--- app/company/components/Academy.tsx
+++ app/company/components/Academy.tsx
@@ -1,14 +1,18 @@
-export default function Academy(){
-    return(
-        <div className="md:flex block">
-            <div className="md:w-1/2">
-           <h1 className="md:font-bold font-semivold md:text-5xl text-2xl md:p-10 p-5">Amanbay IT Academy</h1>
-           <p className="font-medium p-5 text-sm md:text-xl md:p-10">IT Amanbay Academy — это современная академия, где мы обучаем молодежь и взрослых актуальным цифровым навыкам: от основ программирования до продвинутой веб-разработки, UI/UX-дизайна и кибербезопасности.</p>
+import Academy from '@/data/Academy.json';
+
+export default function AcademyPage() {
+  return (
+    <div className="md:flex block">
+      {Academy.map((item, index) => (
+        <div key={index} className="md:w-1/2">
+          <h1 className="md:font-bold font-semibold md:text-5xl text-2xl md:p-10 p-5">
+            {item.title}
+          </h1>
+          <p className="font-medium p-5 text-sm md:text-xl md:p-10">
+            {item.text}
+          </p>
         </div>
-        <div className="md:w-1/2">
-            <h1 className="md:font-bold font-semivold md:text-5xl text-2xl md:p-10 p-5">Наша цель</h1>
-            <p className="font-medium p-5 text-sm md:text-xl md:p-10">Вырастить поколение digital-профессионалов в Казахстане и за его пределами. Мы верим, что каждый может стать частью IT, если ему дать шанс, знания и поддержку. Образование — это инвестиция в будущее.</p>
-        </div>
-        </div>
-    )
+      ))}
+    </div>
+  );
 }
