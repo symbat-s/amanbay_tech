b13fb1a046eafd3d065b27e5cfc4b3a4a3355e1b
symbat-s <seidahmetovberik1983@github.com>
2025-08-27 16:29:01 +0500

    neew

--- app/outsourcing/page.tsx
+++ app/outsourcing/page.tsx
@@ -1,12 +1,14 @@
 import BannerOut  from "./BannerOut"
 import ServiceOut from "./ServiceOut"
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 export default function () {
     return(
         <div>
             <Header/>
             <BannerOut/>
             <ServiceOut/>
+            <Footer/>
         </div>
     )
 }
