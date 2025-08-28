138d8d09e9353b33178bfe716a11f6035d00e13d
symbat-s <seidahmetovberik1983@github.com>
2025-08-28 23:00:25 +0500

    new

--- app/outsourcing/ServiceOut.tsx
+++ app/outsourcing/ServiceOut.tsx
@@ -3,32 +3,32 @@ import ServiceInfoOut from '@/data/ServiceInfoOut.json';
 export default function ServiceOut() {
   const {title , description , foto} = ServiceInfoOut[0]
   return (
-    <div className="w-full mt-20 h-200">
+    <div className="w-full mt-20 h-150 sm:h-200 md:h-280">
         <div className="w-full h-50 text-center  bg-[#404040] pt-10 text-semibold">
-          <h1 className="text-6xl text-white ">Преимущества работы с <br /> Amanbay Tech</h1>
+          <h1 className=" text-white text-3xl mt-5 md:mt-0 sm:mt-0  sm:text-4xl md:text-5xl lg:text-6xl ">Преимущества работы с <br /> Amanbay Tech</h1>
         </div>
 
-        <div className="flex ml-30 mt-10">
-          <img src={foto.src} alt={foto.alt} className="w-160 h-150" />
+        <div className="flex sm:p-20 md:p-30 mt-20 sm:mt-10">
+          <img src={foto.src} alt={foto.alt} className="w-65 h-65  sm:w-100 sm:h-100 md:w-160 md:h-150 " />
             <div className="text-[#404040]">
-              <div className="mt-5">
-                <h1 className="text-2xl font-semibold ">{title.Title1}</h1>
-                <p className="font-light">{description.description1}</p>
+              <div className="mt-2  sd:mt-5 md:mt-5 pr-10 sd:pr-0 md:pr-0">
+                <h1 className="md:text-2xl sm:text-xl text-[10px] font-semibold ">{title.Title1}</h1>
+                <p className="font-light text-[5px] sm:text-[12px] md:text-xl">{description.description1}</p>
               </div>
 
-              <div className="mt-24">
-                <h1 className="text-2xl font-semibold ">{title.Title2}</h1>
-                <p className="font-light">{description.description2}</p>
+              <div className="mt-5 sd:mt-25 md:mt-25 pr-10 sd:pr-0 md:pr-0">
+                <h1 className="md:text-2xl sm:text-xl font-semibold  text-[10px]">{title.Title2}</h1>
+                <p className="font-light  text-[5px] sm:text-[12px] md:text-xl">{description.description2}</p>
               </div>
 
-              <div className="mt-18">
-                <h1 className="text-2xl font-semibold ">{title.Title3}</h1>
-                <p className="font-light">{description.description3}</p>
+              <div className="mt-5 sd:mt-15 md:mt-15 pr-10 sd:pr-0 md:pr-0">
+                <h1 className="md:text-2xl sm:text-xl font-semibold  text-[10px]">{title.Title3}</h1>
+                <p className="font-light text-[5px] sm:text-[12px] md:text-xl">{description.description3}</p>
               </div>
 
-              <div className="mt-17">
-                <h1 className="text-2xl font-semibold ">{title.Title4}</h1>
-                <p className="font-light">{description.description4}</p>
+              <div className="mt-4 sd:mt-18 md:mt-18 pr-10 sd:pr-0 md:pr-0">
+                <h1 className="md:text-2xl sm:text-xl font-semibold text-[10px]">{title.Title4}</h1>
+                <p className="font-light text-[5px] sm:text-[12px] md:text-xl">{description.description4}</p>
               </div>
             </div>
         </div>
