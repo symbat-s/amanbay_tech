dca196aa9f996e1738013ba9c612215e135a02d5
symbat-s <seidahmetovberik1983@github.com>
2025-08-29 10:42:27 +0500

    new

--- app/outsourcing/ServiceOut.tsx
+++ app/outsourcing/ServiceOut.tsx
@@ -13,22 +13,22 @@ export default function ServiceOut() {
             <div className="text-[#404040]">
               <div className="sd:mt-5 md:mt-5  sd:pr-0 md:pr-0">
                 <h1 className="md:text-2xl sm:text-xl text-[10px] font-semibold ">{title.Title1}</h1>
-                <p className="w-30 md:w-140 sm:w-140 font-light text-[8px] sm:text-[12px] md:text-xl">{description.description1}</p>
+                <p className="  font-light text-[8px] sm:text-[12px] md:text-xl">{description.description1}</p>
               </div>
 
               <div className="mt-7 sd:mt-24 md:mt-24 pr-10 sd:pr-0 md:pr-0">
                 <h1 className="md:text-2xl sm:text-xl font-semibold  text-[10px]">{title.Title2}</h1>
-                <p className="font-light w-30 md:w-140 sm:w-140 text-[8px] sm:text-[12px] md:text-xl">{description.description2}</p>
+                <p className="font-light   text-[8px] sm:text-[12px] md:text-xl">{description.description2}</p>
               </div>
 
               <div className="mt-4 sd:mt-15 md:mt-15 pr-10 sd:pr-0 md:pr-0">
                 <h1 className="md:text-2xl sm:text-xl font-semibold  text-[10px]">{title.Title3}</h1>
-                <p className="font-light w-30 md:w-140 sm:w-140 text-[8px] sm:text-[12px] md:text-xl">{description.description3}</p>
+                <p className="font-light  text-[8px] sm:text-[12px] md:text-xl">{description.description3}</p>
               </div>
 
               <div className="mt-2 sd:mt-18 md:mt-18 pr-10 sd:pr-0 md:pr-0">
                 <h1 className="md:text-2xl sm:text-xl font-semibold text-[10px]">{title.Title4}</h1>
-                <p className="font-light w-30 md:w-140 sm:w-140 text-[8px] sm:text-[12px] md:text-xl">{description.description4}</p>
+                <p className="font-light  text-[8px] sm:text-[12px] md:text-xl">{description.description4}</p>
               </div>
             </div>
         </div>
